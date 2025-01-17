dd577688581779e51d73adfbc14df767d48763b6
unknown <dickensallinone@gmail.com>
2025-01-17 23:34:56 +0800

    add new changes. remove some files and update animation

--- assets/functionality/controller.js
+++
@@ -1,5 +0,0 @@
-import { clipboardFuncHandler } from "./clipboard/clipboard.js";
-const sectionFour = document.querySelector(".section--4");
- 
-sectionFour.addEventListener("click",clipboardFuncHandler); 
-
